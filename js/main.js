6c5ed478c495ad5a9b44a73baec229cf84c17a06
ismoilll <turakhodjayev03@gmail.com>
2022-11-14 09:33:45 +0500

    finished post method with toastify

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ getUser();
 
 const tBody = document.querySelector(".t-body");
 const sendForm = document.querySelector("#sendForm");
+const elToastify = document.querySelector(".toastify");
+const elAddButton = document.querySelector("#add-button");
 
 function renderUsers(data = []) {
   data.length > 0
@@ -18,10 +20,10 @@ function renderUsers(data = []) {
           "tr",
           "item",
           `<td>${user.id}</td>
-        <td>${user.user_name}</td>
-        <td>${user.user_score}</td>
-        <td><button class="btn btn-primary data-edit = ${user.id} w-100">EDIT</button></td>
-        <td><button class="btn btn-danger data-del = ${user.id} w-100">DELETE</button></td>`
+      <td>${user.user_name}</td>
+      <td>${user.user_score}</td>
+      <td><button class="btn btn-primary data-edit = ${user.id} w-100">EDIT</button></td>
+      <td><button class="btn btn-danger data-del = ${user.id} w-100">DELETE</button></td>`
         );
 
         tBody.append(tr);
@@ -36,17 +38,32 @@ const addUser = () => {
   const userScore = document.querySelector("#user_score");
   userScore.value.trim();
 
-  if (userName.length === 0 || userScore.length === 0) {
-    alert("please fill in the user name and score");
+  if (userScore.value.length === 0 || userName.value.length === 0) {
+    elToastify.style.backgroundColor = "red";
+    elToastify.innerHTML = `<h5>Ma'lumot yetarli emas ! </h5>`;
+    elToastify.style.transform = "translateX(0)";
+    setTimeout(() => {
+      elToastify.style.transform = "translateX(200%)";
+    }, 1500);
   } else {
-    fetch("http://localhost:8883/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        user_name: userName.value,
-        user_score: +userScore.value,
-      }),
-    });
+    elAddButton.disabled = true;
+
+    elToastify.style.backgroundColor = "lime";
+    elToastify.innerHTML = `<h5>Muvaffaqiyatli qo'shildi ! </h5>`;
+    elToastify.style.transform = "translateX(0)";
+    setTimeout(() => {
+      elToastify.style.transform = "translateX(200%)";
+
+      fetch("http://localhost:8883/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          user_name: userName.value,
+          user_score: +userScore.value,
+        }),
+      });
+      elAddButton.disabled = false;
+    }, 2000);
   }
 };
 sendForm.addEventListener("submit", () => {
